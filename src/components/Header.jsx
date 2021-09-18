666a81cf5f48985d09a34672985d8f244e506e06
shutopia <finoqkrtlgud@naver.com>
2021-09-18 15:47:51 +0900

    상품리스트보여주기

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="header__container">
+        <div className="title">shutopiashop</div>
+        <div className="subtitle">reactweb front-end</div>
+        <div className="btn__area">
+          <a href="https://www.protopie.io" target="_blank" rel="noreferrer">
+            <button>click!</button>
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/components/Orders.jsx
@@ -0,0 +1,10 @@
+export default function Orders() {
+  return (
+    <aside>
+      <div className="empty">
+        <div className="title">you don't have any orders</div>
+        <div className="subtitle">click on a + to add an orders</div>
+      </div>
+    </aside>
+  )
+}
